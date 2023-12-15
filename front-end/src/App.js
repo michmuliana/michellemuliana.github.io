1c6d466652673f6e56d08e7340b449c4f467ab10
Michelle <michmuliana@gmail.com>
2023-12-15 13:25:24 -0500

    added framer motion animation onto the card components and made the card translucent

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,6 +19,7 @@ import {
 } from "@mui/icons-material";
 import "./global.css";
 import ChangeTextComponent from "./change-text";
+import { motion } from "framer-motion";
 
 function App() {
   const circleButtonStyle = {
@@ -39,186 +40,246 @@ function App() {
   ), url(${process.env.PUBLIC_URL}/asset/background-image.jpeg)`;
 
   return (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      style={{
-        minHeight: "100vh",
-        background: backgroundImage,
-        backgroundSize: "cover",
-        padding: "50px",
-      }}
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
     >
-      <Card
-        sx={{
-          width: "500px",
-          height: "445px",
-          borderRadius: "8px",
-          overflow: "hidden",
-          position: "relative",
-          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.3)",
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{
+          minHeight: "100vh",
+          background: backgroundImage,
+          backgroundSize: "cover",
+          padding: "50px",
         }}
       >
-        <CardHeader
-          sx={{ backgroundColor: "#4E7EF5", borderBottom: "2px solid #3366CC" }}
-          action={
-            <div style={{ display: "flex" }}>
-              <IconButton
-                sx={{
-                  backgroundColor: "#F66058",
-                  ":hover": {
-                    backgroundColor: "#F66058",
-                  },
-                  ...circleButtonStyle,
-                }}
-                size="small"
-              />
-              <IconButton
-                sx={{
-                  backgroundColor: "#FBBD2E",
-                  ":hover": {
-                    backgroundColor: "#FBBD2E",
-                  },
-                  ...circleButtonStyle,
-                }}
-                size="small"
-              />
-              <IconButton
-                sx={{
-                  backgroundColor: "#45CA40",
-                  ":hover": {
-                    backgroundColor: "#45CA40",
-                  },
-                  ...circleButtonStyle,
-                }}
-                size="small"
-              />
-            </div>
-          }
-        />
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{
-            padding: "50px",
-            backgroundColor: "rgba(255, 255, 255, 0.7)",
-          }}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.3 }}
         >
-          <Avatar
-            src={process.env.PUBLIC_URL + "./asset/dev-avatar.jpeg"}
-            alt="Michelle Muliana"
+          <Card
             sx={{
-              width: 130,
-              height: 130,
-              marginBottom: 4,
-              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",
+              width: "500px",
+              height: "445px",
+              borderRadius: "8px",
+              overflow: "hidden",
               position: "relative",
-            }}
-          />
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{
-              fontFamily: "Montserrat, sans-serif",
-              color: "black",
-              fontSize: "1.5rem",
-              fontWeight: "bold",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.3)",
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              backdropFilter: "blur(50px)",
+              opacity: 0.9,
             }}
           >
-            Michelle Muliana
-          </Typography>
-          <ChangeTextComponent />
-          <div style={{ display: "flex", gap: "8px" }}>
-            <a
-              href="https://github.com/michmuliana"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <IconButton
-                sx={{
-                  color: "grey",
-                  outline: "2px solid #B4B4B4",
-                  ":hover": {
-                    color: "#4E7EF5",
-                    backgroundColor: "#eee",
-                    outline: "2px solid #4E7EF5",
-                    borderRadius: "50%",
-                  },
-                  ...socialButtonStyle,
-                }}
-              >
-                <GitHub sx={{ fontSize: "30px" }} />
-              </IconButton>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/michellemuliana/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <IconButton
-                sx={{
-                  color: "grey",
-                  outline: "2px solid #B4B4B4",
-                  ":hover": {
-                    color: "#4E7EF5",
-                    backgroundColor: "#eee",
-                    outline: "2px solid #4E7EF5",
-                    borderRadius: "50%",
-                  },
-                  ...socialButtonStyle,
-                }}
-              >
-                <LinkedIn sx={{ fontSize: "30px" }} />
-              </IconButton>
-            </a>
-            <a href="mailto:michmuliana@gmail.com">
-              <IconButton
-                sx={{
-                  color: "grey",
-                  outline: "2px solid #B4B4B4",
-                  ":hover": {
-                    color: "#4E7EF5",
-                    backgroundColor: "#eee",
-                    outline: "2px solid #4E7EF5",
-                    borderRadius: "50%",
-                  },
-                  ...socialButtonStyle,
-                }}
-              >
-                <MailOutline sx={{ fontSize: "30px" }} />
-              </IconButton>
-            </a>
-            <a
-              href={
-                process.env.PUBLIC_URL +
-                "./resumes/Muliana_Michelle_Resume_2023.pdf"
+            <CardHeader
+              sx={{
+                backgroundColor: "#4E7EF5",
+                borderBottom: "2px solid #3366CC",
+              }}
+              action={
+                <div style={{ display: "flex" }}>
+                  <IconButton
+                    sx={{
+                      backgroundColor: "#F66058",
+                      ":hover": {
+                        backgroundColor: "#F66058",
+                      },
+                      ...circleButtonStyle,
+                    }}
+                    size="small"
+                  />
+                  <IconButton
+                    sx={{
+                      backgroundColor: "#FBBD2E",
+                      ":hover": {
+                        backgroundColor: "#FBBD2E",
+                      },
+                      ...circleButtonStyle,
+                    }}
+                    size="small"
+                  />
+                  <IconButton
+                    sx={{
+                      backgroundColor: "#45CA40",
+                      ":hover": {
+                        backgroundColor: "#45CA40",
+                      },
+                      ...circleButtonStyle,
+                    }}
+                    size="small"
+                  />
+                </div>
               }
-              target="_blank"
-              rel="noopener noreferrer"
+            />
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              style={{
+                padding: "50px",
+                backgroundColor: "rgba(255, 255, 255, 0.7)",
+              }}
             >
-              <IconButton
-                sx={{
-                  color: "grey",
-                  outline: "2px solid #B4B4B4",
-                  ":hover": {
-                    color: "#4E7EF5",
-                    backgroundColor: "#eee",
-                    outline: "2px solid #4E7EF5",
-                    borderRadius: "50%",
-                  },
-                  ...socialButtonStyle,
-                }}
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.6 }}
               >
-                <Description sx={{ fontSize: "30px" }} />
-              </IconButton>
-            </a>
-          </div>
-        </Grid>
-      </Card>
-    </Grid>
+                <Avatar
+                  src={process.env.PUBLIC_URL + "./asset/dev-avatar.jpeg"}
+                  alt="Michelle Muliana"
+                  sx={{
+                    width: 130,
+                    height: 130,
+                    marginBottom: 4,
+                    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",
+                    position: "relative",
+                  }}
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.9 }}
+              >
+                <Typography
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    fontFamily: "Montserrat, sans-serif",
+                    color: "black",
+                    fontSize: "1.5rem",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Michelle Muliana
+                </Typography>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.9 }}
+              >
+                <ChangeTextComponent />
+              </motion.div>
+              <div style={{ display: "flex", gap: "8px" }}>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.6, delay: 1.2 }}
+                >
+                  <a
+                    href="https://github.com/michmuliana"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <IconButton
+                      sx={{
+                        color: "grey",
+                        outline: "2px solid #B4B4B4",
+                        ":hover": {
+                          color: "#4E7EF5",
+                          backgroundColor: "#eee",
+                          outline: "2px solid #4E7EF5",
+                          borderRadius: "50%",
+                        },
+                        ...socialButtonStyle,
+                      }}
+                    >
+                      <GitHub sx={{ fontSize: "30px" }} />
+                    </IconButton>
+                  </a>
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.6, delay: 1.2 }}
+                >
+                  <a
+                    href="https://www.linkedin.com/in/michellemuliana/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <IconButton
+                      sx={{
+                        color: "grey",
+                        outline: "2px solid #B4B4B4",
+                        ":hover": {
+                          color: "#4E7EF5",
+                          backgroundColor: "#eee",
+                          outline: "2px solid #4E7EF5",
+                          borderRadius: "50%",
+                        },
+                        ...socialButtonStyle,
+                      }}
+                    >
+                      <LinkedIn sx={{ fontSize: "30px" }} />
+                    </IconButton>
+                  </a>
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.6, delay: 1.2 }}
+                >
+                  <a href="mailto:michmuliana@gmail.com">
+                    <IconButton
+                      sx={{
+                        color: "grey",
+                        outline: "2px solid #B4B4B4",
+                        ":hover": {
+                          color: "#4E7EF5",
+                          backgroundColor: "#eee",
+                          outline: "2px solid #4E7EF5",
+                          borderRadius: "50%",
+                        },
+                        ...socialButtonStyle,
+                      }}
+                    >
+                      <MailOutline sx={{ fontSize: "30px" }} />
+                    </IconButton>
+                  </a>
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.6, delay: 1.2 }}
+                >
+                  <a
+                    href={
+                      process.env.PUBLIC_URL +
+                      "./resumes/Muliana_Michelle_Resume_2023.pdf"
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <IconButton
+                      sx={{
+                        color: "grey",
+                        outline: "2px solid #B4B4B4",
+                        ":hover": {
+                          color: "#4E7EF5",
+                          backgroundColor: "#eee",
+                          outline: "2px solid #4E7EF5",
+                          borderRadius: "50%",
+                        },
+                        ...socialButtonStyle,
+                      }}
+                    >
+                      <Description sx={{ fontSize: "30px" }} />
+                    </IconButton>
+                  </a>
+                </motion.div>
+              </div>
+            </Grid>
+          </Card>
+        </motion.div>
+      </Grid>
+    </motion.div>
   );
 }
 
